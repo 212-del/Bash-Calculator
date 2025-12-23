a99a5e4958e263638e4bee21dce49605ad23d5db
212-del <74853481+212-del@users.noreply.github.com>
2025-12-24 00:00:35 +0530

    The main FIle of Bash-calculator FIle.

---
+++ bash-calculator.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo -e  "\e[31;23;222m A Menu Based Calculator. \e[0m"
+echo -e "\e[31;3;134m Below  operatation can be performed using operators.\n{1} Add\n{2} Subtract\n{3} Multiply\n{4} Division\n{5} Exponentiation(Number1 ≤ 3037000499 & Number2 ≥ 2)\n{6} Exit. \e[0m"
+until [[ $number3 =~ ^-?[0-9]+$ ]]; do
+ read -r -p "   Enter Number1: " number3
+done
+read -r -p  "   Enter Choice: " Choice1
+until [[ $number4 =~ ^-?[0-9]+$ ]]; do
+ read -r -p "   Enter Number2: " number4
+done
+case ${Choice1} in
+ 1)
+  echo "${number3} + ${number4} = "$((${number3} + ${number4}))
+ ;;
+ 2)
+  echo "${number3} - ${number4} = "$((number3 - number4))
+ ;;
+ 3)
+  echo "${number3} * ${number4} = "$((number3 * number4))
+ ;;
+ 4)
+  echo "${number3} ÷ ${number4} = "$((number3 / number4))
+ ;;
+ 5)
+  if [[ $number3 -le 3037000499 ]]; then
+    if [[ $number4 -ge 2 ]]; then
+        echo "${number3}^${number4} = "$((number3 ** number4))
+    else
+        echo "Number1 is Invalid"
+    fi
+  fi
+ ;;
+ 6)
+  exit
+ ;;
+esac
