bb6b8585570defcd374e4e861c2ed39f525eaeeb
212-del <manjaykumars212@gmail.com>
2026-02-10 09:02:18 +0530

    Improved stuffs

--- bash-calculator.sh
+++ bash-calculator.sh
@@ -1,61 +1,85 @@
 #!/bin/bash
 sudo apt install bc || pkg install bc
 echo -e  "\e[36;1;40m A Menu Based Calculator. \e[0m"
-echo -e "\e[36;1;40m Below  operatation can be performed using operators.\n{1} Add\n{2} Subtract\n{3} Multiply\n{4} Division\n{5} Exponentiation(Number1 ≤ 3037000499 & Number2 ≥ 2)\n{6} GCD\n{7} ᵏ√n(ᵏ = number2)\n{8} Exit. \e[0m"
-until [[ $number3 =~ ^-?[0-9]+$ ]]; do
- read -r -p "   Enter Number1: " number3
+echo -e "\e[36;1;40m Below  operatation can be performed using operators.\n{1} Add\n{2} Subtract\n{3} Multiply\n{4} Division\n{5} Exponentiation(Number1 ≤ 3037000499 & Number2 ≥ 2)\n{6} GCD\n{7} ᵏ√n(ᵏ = number2)<Limit =10k>\n{8} Exit. \e[0m"
+until [[ $number1 =~ ^-?[0-9]+$ ]]; do
+ read -r -p "   Enter Number1: " number1
 done
 read -r -p  "   Enter Choice: " Choice1
-until [[ $number4 =~ ^-?[0-9]+$ ]]; do
- read -r -p "   Enter Number2: " number4
+until [[ $number2 =~ ^-?[0-9]+$ ]]; do
+ read -r -p "   Enter Number2: " number2
 done
 case ${Choice1} in
  1)
-  echo "${number3} + ${number4} = "$((${number3} + ${number4}))
+  echo "${number1} + ${number2} = "$((${number1} + ${number2}))
  ;;
  2)
-  echo "${number3} - ${number4} = "$((number3 - number4))
+  echo "${number1} - ${number2} = "$((number1 - number2))
  ;;
  3)
-  echo "${number3} * ${number4} = "$((number3 * number4))
+  echo "${number1} * ${number2} = "$((number1 * number2))
  ;;
  4)
-  echo "${number3} ÷ ${number4} = "$(echo "scale=2; $number3 / $number4" | bc)
+  echo "${number1} ÷ ${number2} = "$(echo "scale=2; $number1 / $number2" | bc)
  ;;
  5)
-  if [[ $number3 -le 3037000499 ]]; then
-    if [[ $number4 -ge 2 ]]; then
-       echo "${number3}^${number4} = "$((number3 ** number4))
-    elif [[ $number4 == "1" ]]; then
-      echo "${number3}^${number4} = "$((number3 ** number4))
-    elif [[ $number4 == "0"  ]]; then
-      echo "${number3}^${number4} = "$((number3 ** number4))  
+  if [[ $number1 -le 3037000499 ]]; then
+    if [[ $number2 -ge 2 ]]; then
+       echo "${number1}^${number2} = "$((number1 ** number2))
+    elif [[ $number2 == "1" ]]; then
+      echo "${number1}^${number2} = "$((number1 ** number2))
+    elif [[ $number2 == "0"  ]]; then
+      echo "${number1}^${number2} = "$((number1 ** number2))  
     else
         echo "Number1 is Invalid"
     fi
   fi
  ;;
  6)
-   if [[ "$number3" -gt "$number4" ]]; then
-    greater_number=$number3
-    smaller_number=$number4
-   elif [[ "$number3" == "$number4"  ]]; then
-    echo "HCF is ${number3}"
+   if [[ "$number1" -gt "$number2" ]]; then
+    greater_number=$number1
+    smaller_number=$number2
+   elif [[ "$number1" == "$number2"  ]]; then
+    echo "HCF is ${number1}"
    else
-    greater_number=$number4
-    smaller_number=$number3
+    greater_number=$number2
+    smaller_number=$number1
    fi
    for i in $(seq 1 "$smaller_number"); do
     temp=$(
-    echo -n $(echo "scale=3; $number3 / $i" | bc) 
-    echo -n "" $(echo "scale=3; $number4 / $i" | bc)
+    echo -n $(echo "scale=3; $number1 / $i" | bc) 
+    echo -n "" $(echo "scale=3; $number2 / $i" | bc)
     echo "" "Divided by $i"
     )
     echo "$temp" | grep -E '(\.000.*\.000)'
    done | echo "HCF= "$(sed -n '$s/.*\([0-9]\+\)$/\1/p')
  ;;
  7)
-  echo "Square Root"
+  assume_num=""
+  while [[ $(echo "$old_threshold != $new_threshold" | bc) -eq 1 ]]; do
+   if [[ $number2 -ge "2" ]]; then
+    assume="" 
+    first_sum(){
+     echo "($number2 -1) * $assume" | bc
+}
+    second_sum(){
+     echo "$number1 / $assume^($number2 -1)" | bc
+}
+    assume=$(echo "scale=3; $(first_sum) + $(second_sum) / $number2" | bc)
+    old_threshold="$assume"
+     first_sum(){
+     echo "($number2 -1) * $assume" | bc
+}
+     second_sum(){ 
+      echo "$number1 / $assume^($number2 -1)" | bc
+ }
+    assume=$(echo "scale=3; $(first_sum) + $(second_sum) / $number2" | bc)
+    new_threshold="$assume"
+   else
+    echo "Number2 > 2"
+   fi
+  sleep 1
+  done
  ;;
  8)
   exit
